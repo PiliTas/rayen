a8cf7d36948523d5654fe07aa9ebe4c842008527
NPGonzalez <naypili@gmail.com>
2020-09-15 15:44:50 -0300

    Se crean los componentes de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditComponent } from './components/edit/edit.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditComponent,
+    NavbarComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
