053ed0976bff284b442ef9e80a7700751cfce0d7
NPGonzalez <naypili@gmail.com>
2020-09-15 18:49:03 -0300

    Se crea servicio para funciones de ordenamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { DataService } from './services/data.service';
-
+import { NumericalService } from './services/numerical.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { EditComponent } from './components/edit/edit.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     HttpClientModule
   ],
   providers: [
-    DataService
+    DataService,
+    NumericalService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/numerical.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NumericalService {
+
+  constructor() { }
+
+// FUNCIÓN PARA ORDENAR POR FECHA
+
+
+
+//FUNCIÓN PARA ORDENAR POR TÍTULO
+
+
+}
